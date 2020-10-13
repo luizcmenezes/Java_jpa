26c795d10c3c739657b1add0ee618d5c1a8a724b
luizcmenezes <lcmscontas@gmail.com>
2020-10-13 16:05:11 -0300

    Relacionamento ManyToMany

---
+++ projeto-jpa/src/main/java/br/com/alura/jpa/modelo/TestaRelacionamentoMovimentacaoCategoria.java
@@ -0,0 +1,48 @@
+package br.com.alura.jpa.modelo;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.Arrays;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class TestaRelacionamentoMovimentacaoCategoria {
+	public static void main(String[] args) {
+		
+		Categoria categoria = new Categoria("Viagem");
+		Categoria categoria2 = new Categoria("Negocios");
+
+		Conta conta = new Conta();
+		conta.setId(2L);
+		
+		Movimentacao movimentacao = new Movimentacao();
+		movimentacao.setDescriscao("Viagem à São Paulo");
+		movimentacao.setTipoMvimentacao(TipoMovimentacao.SAIDA);
+		movimentacao.setData(LocalDateTime.now());
+		movimentacao.setValor(new BigDecimal(300.0));
+		movimentacao.setCategoria(Arrays.asList(categoria, categoria2));
+		movimentacao.setConta(conta);
+		
+		Movimentacao movimentacao2 = new Movimentacao();
+		movimentacao2.setDescriscao("Viagem ao Rio de Janeiro");
+		movimentacao2.setTipoMvimentacao(TipoMovimentacao.SAIDA);
+		movimentacao2.setData(LocalDateTime.now());
+		movimentacao2.setValor(new BigDecimal(400.0));
+		movimentacao2.setCategoria(Arrays.asList(categoria, categoria2));
+		movimentacao2.setConta(conta);
+		
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("alura");
+		EntityManager em = emf.createEntityManager();
+		
+		em.getTransaction().begin();
+		em.persist(categoria);
+		em.persist(categoria2);
+		em.persist(movimentacao);
+		em.persist(movimentacao2);
+		em.getTransaction().commit();
+		em.close();
+		
+	}
+}

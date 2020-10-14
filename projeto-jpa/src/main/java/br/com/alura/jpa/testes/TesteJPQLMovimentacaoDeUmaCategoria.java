4e615dfd091529e8a1ba16177b8b820b881c50ed
luizcmenezes <lcmscontas@gmail.com>
2020-10-13 21:32:01 -0300

    Aula JPQL

--- projeto-jpa/src/main/java/br/com/alura/jpa/testes/TesteJPQLMovimentacaoDeUmaCategoria.java
+++ projeto-jpa/src/main/java/br/com/alura/jpa/testes/TesteJPQLMovimentacaoDeUmaCategoria.java
@@ -7,27 +7,28 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import javax.persistence.TypedQuery;
 
-import br.com.alura.jpa.modelo.Conta;
+import br.com.alura.jpa.modelo.Categoria;
 import br.com.alura.jpa.modelo.Movimentacao;
 
-public class TesteJPQL {
+public class TesteJPQLMovimentacaoDeUmaCategoria {
 
 	public static void main(String[] args) {
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("alura");
 		EntityManager em = emf.createEntityManager();
 
-		Conta conta = new Conta();
-		conta.setId(2L);
-		String sql = "select m from Movimentacao m where m.conta = :pConta order by m.valor desc";
+		Categoria categoria = new Categoria();
+		categoria.setId(2L);
+		String sql = "select m from Movimentacao m join m.categoria c where c = :pCategoria";
 
 		TypedQuery<Movimentacao> query = em.createQuery(sql, Movimentacao.class);
-		query.setParameter("pConta", conta);
+		query.setParameter("pCategoria", categoria);
 		List<Movimentacao> resultList = query.getResultList();
 
 		if (resultList.size() > 0) {
 			System.out.println();
 			for (Movimentacao movimentacao : resultList) {
 				System.out.println("------------------------------------------");
+				System.out.println("Categorias: " + movimentacao.getCategoria());
 				System.out.println("Descrição: " + movimentacao.getDescriscao());
 				System.out.println("Valor: " + movimentacao.getValor());
 				System.out.println("Tipo: " + movimentacao.getTipoMvimentacao());
@@ -35,7 +36,5 @@ public class TesteJPQL {
 		} else {
 			System.out.println("Nenhum resultado.");
 		}
-
 	}
-
 }

4455c83dc8d9aa3c7033111f253db373dd76f9f1
luizcmenezes <lcmscontas@gmail.com>
2020-10-19 20:58:06 -0300

    Aula sobre jpql organizando código

---
+++ 1743-jpa-pesquisas-projeto_inicial/src/main/java/br/com/alura/jpa/testes/TesteMediaDiariaDasMovimentacoes.java
@@ -0,0 +1,28 @@
+package br.com.alura.jpa.testes;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+
+public class TesteMediaDiariaDasMovimentacoes {
+
+	public static void main(String[] args) {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("alura");
+		EntityManager em = emf.createEntityManager();
+
+		String jpql = "select avg(m.valor), day(m.data), month(m.data) from Movimentacao m group by day(m.data), month(m.data), year(m.data)";
+
+		Query query = em.createQuery(jpql);
+		List<Object[]> somaDasMovimentacoes = query.getResultList();
+
+		System.out.println();
+		for (Object[] resultado : somaDasMovimentacoes) {
+			System.out.println("A média das movimentações do dia: " + resultado[1] + "/" + resultado[2] + " é: " + resultado[0]);
+		}
+
+	}
+
+}

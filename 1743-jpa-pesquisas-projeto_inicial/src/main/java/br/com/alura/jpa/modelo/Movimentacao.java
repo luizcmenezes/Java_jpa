3eb870023db112229d4ec9e102d9f14862465d19
luizcmenezes <lcmscontas@gmail.com>
2020-10-19 11:58:42 -0300

    Aula query sum e avg

--- 1743-jpa-pesquisas-projeto_inicial/src/main/java/br/com/alura/jpa/modelo/Movimentacao.java
+++ 1743-jpa-pesquisas-projeto_inicial/src/main/java/br/com/alura/jpa/modelo/Movimentacao.java
@@ -2,76 +2,86 @@ package br.com.alura.jpa.modelo;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.Entity;
-import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Movimentacao {
-
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	@Enumerated(EnumType.STRING)
-	private TipoMovimentacao tipoMvimentacao;
+	@Enumerated
+	private TipoMovimentacao tipoMovimentacao;
 	private LocalDateTime data;
-	private String descriscao;
+	private String descricao;
 	private BigDecimal valor;
-	
 	@ManyToOne
 	private Conta conta;
+	@OneToMany
+	private List<Categoria> categorias = new ArrayList<>();
 	
-	@ManyToMany
-	private List<Categoria> categoria;
-	
-	public Long getId() {
-		return id;
+	public List<Categoria> getCategorias() {
+		return categorias;
 	}
-	public void setId(Long id) {
-		this.id = id;
+
+	public void setCategorias(List<Categoria> categorias) {
+		this.categorias = categorias;
 	}
-	public TipoMovimentacao getTipoMvimentacao() {
-		return tipoMvimentacao;
-	}
-	public void setTipoMvimentacao(TipoMovimentacao tipoMvimentacao) {
-		this.tipoMvimentacao = tipoMvimentacao;
-	}
-	public LocalDateTime getData() {
-		return data;
-	}
-	public void setData(LocalDateTime data) {
-		this.data = data;
-	}
-	public String getDescriscao() {
-		return descriscao;
-	}
-	public void setDescriscao(String descriscao) {
-		this.descriscao = descriscao;
-	}
-	public BigDecimal getValor() {
-		return valor;
-	}
-	public void setValor(BigDecimal valor) {
-		this.valor = valor;
-	}
+
 	public Conta getConta() {
 		return conta;
 	}
+
 	public void setConta(Conta conta) {
 		this.conta = conta;
 	}
-	public List<Categoria> getCategoria() {
-		return categoria;
+
+	public Long getId() {
+		return id;
 	}
-	public void setCategoria(List<Categoria> categoria) {
-		this.categoria = categoria;
+
+	public void setId(Long id) {
+		this.id = id;
 	}
-	
+
+	public TipoMovimentacao getTipoMovimentacao() {
+		return tipoMovimentacao;
+	}
+
+	public void setTipoMovimentacao(TipoMovimentacao tipoMovimentacao) {
+		this.tipoMovimentacao = tipoMovimentacao;
+	}
+
+	public LocalDateTime getData() {
+		return data;
+	}
+
+	public void setData(LocalDateTime data) {
+		this.data = data;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public BigDecimal getValor() {
+		return valor;
+	}
+
+	public void setValor(BigDecimal valor) {
+		this.valor = valor;
+	}
+
 }
---
+++ 1743-jpa-pesquisas-projeto_inicial/src/main/java/br/com/alura/jpa/testes/TesteMediaDiariaDasMovimentacoes.java
@@ -0,0 +1,29 @@
+package br.com.alura.jpa.testes;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+
+public class TesteMediaDiariaDasMovimentacoes {
+
+	public static void main(String[] args) {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("alura");
+		EntityManager em = emf.createEntityManager();
+		
+		String jpql = "select avg(m.valor) from Movimentacao m group by day(m.data), month(m.data), year(m.data)";
+		
+		TypedQuery<Double> query = em.createQuery(jpql, Double.class);
+		List<Double> somaDasMovimentacoes = query.getResultList();
+		
+		System.out.println();
+		for (Double media : somaDasMovimentacoes) {
+			System.out.println("A média das movimentações é: " + media);
+		}
+		
+
+	}
+
+}

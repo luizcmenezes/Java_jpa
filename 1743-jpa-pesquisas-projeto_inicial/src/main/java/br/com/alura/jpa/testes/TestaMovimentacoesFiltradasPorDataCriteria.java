1ad1c1ae61c8be5a8c2ca4937a9e3a43031408e3
luizcmenezes <lcmscontas@gmail.com>
2020-10-20 10:28:20 -0300

    Queries dinâmicas com criteria

---
+++ 1743-jpa-pesquisas-projeto_inicial/src/main/java/br/com/alura/jpa/testes/TestaMovimentacoesFiltradasPorDataCriteria.java
@@ -0,0 +1,29 @@
+package br.com.alura.jpa.testes;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import br.com.alura.jpa.modelo.Movimentacao;
+import br.com.alura.jpa.modelo.dao.MovimentacaoDao;
+
+public class TestaMovimentacoesFiltradasPorDataCriteria {
+
+	public static void main(String[] args) {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("alura");
+		EntityManager em = emf.createEntityManager();
+		
+		MovimentacaoDao movimentacaoDao = new MovimentacaoDao(em);
+		List<Movimentacao> movimentacaoFiltradasPorData = movimentacaoDao.getMovimentacaoFiltradasPorData(17, 10, 2020);
+		
+		for (Movimentacao movimentacao : movimentacaoFiltradasPorData) {
+			System.out.println("Descrição -> " + movimentacao.getDescricao());
+			System.out.println("Valor -> " + movimentacao.getValor());
+			System.out.println("-------------------------------------------");
+		}
+		
+	}
+
+}

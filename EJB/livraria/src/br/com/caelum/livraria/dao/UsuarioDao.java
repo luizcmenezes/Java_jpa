8038432a4b19a5e8a359dfdec71a5cb69786df95
luizcmenezes <lcmscontas@gmail.com>
2020-10-21 19:49:44 -0300

    03. Integração do JPA com Pool e DataSource

--- EJB/livraria/src/br/com/caelum/livraria/dao/UsuarioDao.java
+++ EJB/livraria/src/br/com/caelum/livraria/dao/UsuarioDao.java
@@ -1,16 +1,22 @@
 package br.com.caelum.livraria.dao;
 
 import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 import br.com.caelum.livraria.modelo.Usuario;
 
 @Stateless
 public class UsuarioDao {
 
-	private Banco banco = new Banco();
+	@PersistenceContext
+	private EntityManager manager;
 
 	public Usuario buscaPeloLogin(String login) {
-		return this.banco.buscaPeloNome(login);
+		Usuario usuario = (Usuario) manager
+				.createQuery("select u from Usuario u where u.login = :pLogin", Usuario.class)
+				.setParameter("pLogin", login).getSingleResult();
+		return usuario;
 	}
-	
+
 }
